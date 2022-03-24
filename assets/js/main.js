c099bef3349dfaa8e0a2149182b14ec0f80f5289
NicolaMigliore <auster.mig@gmail.com>
2022-03-24 16:19:17 +0100

    add code in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,58 @@
 
 //chiedere all'utente il numero di km che vuole percorrere
 
-const tratta = parseInt ()
+const tratta = parseInt(prompt("Quanti Km vuoi percorrere?"));
+console.log(tratta);
+
+if (isNaN(tratta)){
+    alert("Attenzione, Inserire un valore numerico");
+}
+
+
+
 
 //chiedere all'utente la sua età
 
-//calcolare prezzo totale del viaggio
+const age = parseInt(prompt("Quanti anni hai?"));
+console.log(age);
+
+if (isNaN(age)) {
+    alert("Attenzione, inserire un valore numerico")
+}
+
+
+//calcolare prezzo intero del viaggio
+
+const price_one_km = 0.21;
+const full_price = tratta * price_one_km ;
+
+if (age < 18){
+    /* il prezzo del biglietto con lo sconto per gli under18 */
+
+    const discount_under18 = full_price * 0.20;
+    const price_under18 = full_price - discount_under18;
+    
+    console.log(`il prezzo del tuo biglietto è ${price_under18} €`);
+} 
+
+else if (age > 65) {
+    /* il prezzo del biglietto con lo sconto per gli over65 */
+    const discount_over65 = full_price * 0.40;
+    const price_over65 = full_price - discount_over65;
+
+    console.log(`Il prezzo del tuo biglietto è ${price_over65} €`);
+}
+
+else {
+    /* il prezzo del biglietto intero */
+    console.log(` Il prezzo del tuo biglietto è ${full_price} €`);
+}
+
+
+
+
+
+
+
+
+
