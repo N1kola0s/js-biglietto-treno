89b5d5bd94fc5aed60770032665bfe409ff791d3
NicolaMigliore <auster.mig@gmail.com>
2022-03-24 13:49:31 +0100

    add comments in js file

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// js-biglietto-treno; DESCRIZIONE:  chiedere all'utente il numero di chilometri che vuole percorrere e la sua età. Sulla base di queste informazioni calcolare il prezzo totale del viaggio, secondo queste regole: - il prezzo del biglietto è definito in base ai km (0.21 € al km) - va applicato uno sconto del 20% per i minorenni e va applicato uno sconto del 40% per gli over 65. Output del prezzo finale va scritto in forma umana (con massmo due deimali, per indicare centesimi sul prezzo).
+
+
+//chiedere all'utente il numero di km che vuole percorrere
+
+const tratta = parseInt ()
+
+//chiedere all'utente la sua età
+
+//calcolare prezzo totale del viaggio
